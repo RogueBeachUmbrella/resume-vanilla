a4cdbcb3d9981945cae0b652fc2d93ce7a2b1bf6
RogueBeachUmbrella <jmgreenlee@outlook.com>
2021-07-26 23:44:13 -0400

    initial commit

---
+++ site.js
@@ -0,0 +1,29 @@
+// function logItem(e) {
+//     const item = document.querySelector(`[data-id=${e.target.id}]`);
+//     item.toggleAttribute('hidden');
+// }
+
+// const chapters = document.querySelectorAll('details');
+// chapters.forEach((chapter) => {
+//     chapter.addEventListener('toggle', logItem);
+// });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function toggleDropdown() {
+    document.getElementById("contact-section").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
