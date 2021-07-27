2c47fd252e9a1cecc9d47dbdafb1a2a9b80980c9
RogueBeachUmbrella <jmgreenlee@outlook.com>
2021-07-27 08:58:59 -0400

    double sticky nav wip

--- site.js
+++ site.js
@@ -10,20 +10,44 @@
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function toggleDropdown() {
-    document.getElementById("contact-section").classList.toggle("show");
-}
+// function toggleDropdown() {
+//     document.getElementById("contact-section").classList.toggle("show");
+// }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
+
+window.onscroll = function() { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var navbar2 = document.getElementById("navbar2");
+var sticky = navbar.offsetTop;
+var sticky2 = navbar2.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+
+    if (window.pageYOffset >= sticky2 - 75) {
+        navbar2.classList.add("sticky2")
+            // navbar2.classList.remove("sticky2");
+
+    } else {
+        navbar2.classList.remove("sticky2");
+        // navbar2.classList.add("unsticky")
     }
 }
