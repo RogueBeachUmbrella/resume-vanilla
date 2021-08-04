13447febec1768418d250fe8f2840cd93989b2f2
RogueBeachUmbrella <jmgreenlee@outlook.com>
2021-08-04 01:20:41 -0400

    wip

--- site.js
+++ site.js
@@ -72,3 +72,21 @@ class StickyNavigation {
 }
 
 new StickyNavigation();
+
+
+jQuery(document).ready(function($) {
+
+    $('.smoothscroll').on('click', function(e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 800, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+
+});
